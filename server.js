47ef6da67845ab7a02e1aa55b648b4c4f439f126
Rahul Bangar <105964685+irahulbangar@users.noreply.github.com>
2025-05-28 12:11:33 +0530

    Add user and hotel management routes, controllers, and middleware
    - Implement user routes for fetching user data and storing recent searched cities.
    - Create hotel routes for registering hotels with authentication middleware.
    - Add controllers for handling user and hotel operations.
    - Introduce authentication middleware to protect routes.
    - Define Hotel model for hotel data management.

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import cors from "cors";
 import connectDB from "./configs/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import clerkWebhooks from "./controllers/clerkWebhooks.js";
+import userRouter from "./routes/userRoutes.js";
+import hotelRouter from "./routes/hotelRoutes.js";
 
 connectDB();
 
@@ -16,6 +18,8 @@ app.use(clerkMiddleware());
 
 // Api to listen to clerk Webhooks
 app.use("/api/clerk", clerkWebhooks);
+app.use("/api/user", userRouter);
+app.use("/api/hotels", hotelRouter);
 
 app.get("/", (req, res) => {
   res.send("API is working");
