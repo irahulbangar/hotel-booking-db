5c44c5c4f60004b82743f60ac129b08ea5e12e35
Rahul Bangar <105964685+irahulbangar@users.noreply.github.com>
2025-05-28 18:05:26 +0530

    Add booking and room management features
    - Implement booking functionality with APIs for checking availability, creating bookings, and retrieving user and hotel bookings.
    - Create room management APIs for adding new rooms, fetching available rooms, and toggling room availability.
    - Integrate Cloudinary for image uploads in room creation.
    - Update server configuration to include new routes and middleware for handling uploads.
    - Add necessary models for Booking and Room management.

--- server.js
+++ server.js
@@ -6,8 +6,12 @@ import { clerkMiddleware } from "@clerk/express";
 import clerkWebhooks from "./controllers/clerkWebhooks.js";
 import userRouter from "./routes/userRoutes.js";
 import hotelRouter from "./routes/hotelRoutes.js";
+import connectCloudinary from "./configs/cloudinary.js";
+import roomRouter from "./routes/roomRoutes.js";
+import bookingRouter from "./routes/bookingRouts.js";
 
 connectDB();
+connectCloudinary();
 
 const app = express();
 app.use(cors()); // Enable cross-origin resource sharing
@@ -20,6 +24,8 @@ app.use(clerkMiddleware());
 app.use("/api/clerk", clerkWebhooks);
 app.use("/api/user", userRouter);
 app.use("/api/hotels", hotelRouter);
+app.use("/api/rooms", roomRouter);
+app.use("/api/bookings", bookingRouter);
 
 app.get("/", (req, res) => {
   res.send("API is working");
